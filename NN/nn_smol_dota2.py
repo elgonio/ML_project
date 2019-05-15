f79c7eea31a43bf94991a38d61f286e15bdc772d
Nandi <1064787@students.wits.ac.za>
2019-05-15 21:37:15 +0200

    Tiny test

---
+++ NN/nn_smol_dota2.py
@@ -0,0 +1,42 @@
+import numpy as np 
+
+from nn_network import Network 
+from nn_fc_layer import FCLayer 
+from nn_activation_layer import ActivationLayer 
+from nn_activation_tan import tanh, tanh_prime 
+from nn_loss import mse, mse_prime
+
+# training data
+x_train = np.array( [ [[2,2,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,-1,0,0,0,-1,0,0,1,0,0,1,0,0,0,1,0,0,0,0,0,-1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1,0,0,0,0,0,0,0,0,0,0,0,0,0,-1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]], 
+                    [[8,2,0,0,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,1,0,0,-1,0,0,0,0,0,-1,0,0,0,0,0,0,0,0,0,0,0,0,0,-1,0,0,0,0,0,0,0,0,0,0,0,0,-1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,-1,0,0,0,0,0,0,0,0,0]], 
+                    [[2,2,1,0,0,0,-1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,-1,0,0,0,-1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]], 
+                    [[2,2,-1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,-1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,-1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,-1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]] ] )
+y_train = np.array( [ [[0]], [[1]], [[0]], [[1]] ] )
+
+# network
+net = Network()
+# number of layers
+num_layers = 100
+num_nodes = 10
+# init layer
+net.add( FCLayer( 115, num_nodes ) )
+net.add( ActivationLayer( tanh, tanh_prime ) )
+# create amount of layers
+for n in range( num_layers - 2 ):
+    net.add( FCLayer( num_nodes, num_nodes ) )
+    net.add( ActivationLayer( tanh, tanh_prime ) )
+
+# final layer
+net.add( FCLayer( num_nodes, 1 ) )
+net.add( ActivationLayer( tanh, tanh_prime ) )
+
+# train
+net.use( mse, mse_prime )
+net.fit( x_train, y_train, epochs = 5000, learning_rate = 0.1 )
+
+# test
+out = net.predict( x_train )
+# f = open( "results.txt", "w+" )
+print( out )
+# f.write( str( out ) )
+# f.close()
