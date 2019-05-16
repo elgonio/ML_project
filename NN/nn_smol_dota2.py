e73ae16a146ad109bd037a9ee479fa1c5030e189
Nandi <1064787@students.wits.ac.za>
2019-05-16 12:33:09 +0200

    My version of thing

--- NN/nn_smol_dota2.py
+++ NN/nn_smol_dota2.py
@@ -6,37 +6,49 @@ from nn_activation_layer import ActivationLayer
 from nn_activation_tan import tanh, tanh_prime 
 from nn_loss import mse, mse_prime
 
+
 # training data
 x_train = np.array( [ [[2,2,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,-1,0,0,0,-1,0,0,1,0,0,1,0,0,0,1,0,0,0,0,0,-1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1,0,0,0,0,0,0,0,0,0,0,0,0,0,-1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]], 
                     [[8,2,0,0,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,1,0,0,-1,0,0,0,0,0,-1,0,0,0,0,0,0,0,0,0,0,0,0,0,-1,0,0,0,0,0,0,0,0,0,0,0,0,-1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,-1,0,0,0,0,0,0,0,0,0]], 
                     [[2,2,1,0,0,0,-1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,-1,0,0,0,-1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]], 
                     [[2,2,-1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,-1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-1,0,0,0,0,0,0,0,0,0,0,0,0,1,0,-1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,-1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]] ] )
-y_train = np.array( [ [[0]], [[1]], [[0]], [[1]] ] )
+y_train = np.array( [ [[-1]], [[1]], [[-1]], [[1]] ] )
 
 # network
+# 3 layers (1 hidden layer)
+num_nodes = 115
 net = Network()
-# number of layers
-num_layers = 100
-num_nodes = 10
 # init layer
 net.add( FCLayer( 115, num_nodes ) )
 net.add( ActivationLayer( tanh, tanh_prime ) )
-# create amount of layers
-for n in range( num_layers - 2 ):
-    net.add( FCLayer( num_nodes, num_nodes ) )
-    net.add( ActivationLayer( tanh, tanh_prime ) )
-
+# hidden layer(s)
+net.add( FCLayer( num_nodes, num_nodes ) )
+net.add( ActivationLayer( tanh, tanh_prime ) )
 # final layer
 net.add( FCLayer( num_nodes, 1 ) )
 net.add( ActivationLayer( tanh, tanh_prime ) )
 
 # train
 net.use( mse, mse_prime )
-net.fit( x_train, y_train, epochs = 5000, learning_rate = 0.1 )
+net.fit( x_train, y_train, epochs = 1000, learning_rate = 0.02 )
 
 # test
-out = net.predict( x_train )
-# f = open( "results.txt", "w+" )
+out = net.predict( x_train )  # replace with test when testing, then check outputs against the validation
+
+# here add the clamping function
+
 print( out )
+
+
+
+# OBSOLETE
+# creating hidden layers
+#for n in range( num_layers - 2 ):
+#    net.add( FCLayer( num_nodes, num_nodes ) )
+#    net.add( ActivationLayer( tanh, tanh_prime ) )
+
+# writing out to file
+# f = open( "results.txt", "w+" )
+# print( out )
 # f.write( str( out ) )
 # f.close()
