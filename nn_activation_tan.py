19db515e2d40ecefeda053c769825097df8a41a7
Nandi <1064787@students.wits.ac.za>
2019-05-20 22:32:50 +0200

    remove legacy files

--- nn_activation_tan.py
+++
@@ -1,9 +0,0 @@
-# tan activation function
-import numpy as np 
-
-# activation function and its derivative
-def tanh( x ):
-    return np.tanh( x )
-
-def tanh_prime( x ):
-    return 1 - np.tanh( x ) ** 2
